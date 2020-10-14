7811d9fc466a70ae32c81d8f6181c92a73624465
Teale Foster <tealefoster@gmail.com>
2020-10-14 16:24:35 -0500

    minor correction to indentation

--- plots.js
+++ plots.js
@@ -31,22 +31,22 @@ function optionChanged(newSample) {
 //   } 
 ///////////////////////////////////////// Original Block ///////////////////////////////////
 
-  function buildMetadata(sample) {
-    d3.json("samples.json").then((data) => {
-      var metadata = data.metadata;
-      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-      var result = resultArray[0];
-      var PANEL = d3.select("#sample-metadata");
-  
-      PANEL.html("");
-      PANEL.append("h6").text(result.id);
-      PANEL.append("h6").text(result.ethnicity);
-      PANEL.append("h6").text(result.gender);
-      PANEL.append("h6").text(result.age);
-      PANEL.append("h6").text(result.location);
-      PANEL.append("h6").text(result.bbtype);
-      PANEL.append("h6").text(result.wfreq);
-    });
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+    PANEL.append("h6").text(result.id);
+    PANEL.append("h6").text(result.ethnicity);
+    PANEL.append("h6").text(result.gender);
+    PANEL.append("h6").text(result.age);
+    PANEL.append("h6").text(result.location);
+    PANEL.append("h6").text(result.bbtype);
+    PANEL.append("h6").text(result.wfreq);
+  });
   }
 
 {/* <select id="selDataset" onchange="optionChanged(this.value)"></select> */}
