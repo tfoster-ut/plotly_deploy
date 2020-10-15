5011cf1d7855982674a3287f1e9946368381db2f
Teale Foster <tealefoster@gmail.com>
2020-10-15 18:52:19 -0500

    begin chart development and add git ignore

--- plots.js
+++ plots.js
@@ -38,16 +38,18 @@ function buildMetadata(sample) {
     var result = resultArray[0];
     var PANEL = d3.select("#sample-metadata");
 
+    // Use `.html("") to clear any existing metadata
     PANEL.html("");
-    PANEL.append("h6").text(result.id);
-    PANEL.append("h6").text(result.ethnicity);
-    PANEL.append("h6").text(result.gender);
-    PANEL.append("h6").text(result.age);
-    PANEL.append("h6").text(result.location);
-    PANEL.append("h6").text(result.bbtype);
-    PANEL.append("h6").text(result.wfreq);
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+
   });
-  }
+}
 
 {/* <select id="selDataset" onchange="optionChanged(this.value)"></select> */}
 
